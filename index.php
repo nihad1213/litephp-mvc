43ae2cd359ebafe7ac086eac6bf90a5150c4ba94
nihad1213 <nihad.nemetli@gmail.com>
2025-06-07 23:22:11 +0400

    update in index.php file

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
+
+// Framework need to be work like that
+// $app = new Application();
+
+// $router = new Router();
+
+// $app->router->get('/', function(){
+
+// });
+
+// $app->run();
+
 echo "test";
